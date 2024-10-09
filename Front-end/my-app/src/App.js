8e6cd293cedcc3df4fee4ed49b2804db260e671e
Kavyan17 <nandk02@pfw.edu>
2024-10-08 21:56:53 -0400

    Added landing page

---
+++ Front-end/my-app/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LandingPage } from './components/LandingPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
